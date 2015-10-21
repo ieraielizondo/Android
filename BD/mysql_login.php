52cf0c8dcc0f070756bfa2b27dd695e5e794259b
Ierai <ierai@Ubuntu-12-04.(none)>
2015-10-21 14:29:58 +0200

    primer commit

---
+++ BD/mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "localhost");// Nombre del host
+define("DATABASE", "f1app"); // Nombre de la base de datos
+define("USERNAME", "f1app"); // Nombre del usuario
+define("PASSWORD", "f1app"); // Nombre de la constraseña
+?>
